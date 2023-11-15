2ed9efa159a8657adb0ba2ac684b5a95ab208bd5
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2023-11-15 18:42:24 -0300

    Fix navlink pagina not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 const NotFound = () => {
-  return (<div>NotFound</div>);
+  return (
+    <div className="container text-center p-5">
+       <h1>Página no encontrada</h1> 
+    </div>);
 };
 
 export default NotFound;
